fc3373b88287c68ec9324ff6f134ede0be6c9e04
Ashton Mares <amares1@stedwards.edu>
2026-01-20 15:54:00 -0600

    Initial backend and frontend setup

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.send("Reservation System Backend Running");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
