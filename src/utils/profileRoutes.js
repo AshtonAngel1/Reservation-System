fcc1e57183e1d7198fcd428daba98d29b5d8b1e2
jackawebby <153349294+jackawebby@users.noreply.github.com>
2026-03-03 14:50:21 -0600

    Update profileRoutes.js

--- src/utils/profileRoutes.js
+++ src/utils/profileRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../db"); // adjust path if your db.js is elsewhere
+const db = require("../db"); 
 
 // Get user profile
 router.get("/", async (req, res) => {
